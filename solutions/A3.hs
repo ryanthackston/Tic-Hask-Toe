f93670fd56627d3349afdf17d174d85a64200171
ryanthackston <34466711+ryanthackston@users.noreply.github.com>
2023-04-15 23:49:58 -0500

    Update A3.hs

--- solutions/A3.hs
+++ solutions/A3.hs
@@ -8,20 +8,25 @@ import Data.List (transpose)
 -- *** Assignment 3-1 ***
 
 -- Q#01
+showInts :: [Int] -> [String]
+showInts [] = []
+showInts (x:xs) = show x : showInts xs
 
-showInts = undefined
-
-
-_HEADER_ = undefined
+_HEADER_ :: String
+_HEADER_ = tail $ tail $ tail (formatLine([[' ']] ++ showInts _RANGE_))
 
 -- Q#02
 
-showSquares = undefined
+showSquares :: [Square] -> [String]
+showSquares (x:xs) = show x : showSquares xs
+showSquares [] = []
 
 
 -- Q#03
 
-formatRows = undefined
+formatRows :: [Row] -> [String]
+formatRows []       = []
+formatRows (x:xs) = formatLine (showSquares x) : formatRows xs
 
 -- Q#04
 
